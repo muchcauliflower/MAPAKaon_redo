2d9240cb6b9846a1b31e21757f1ed2f634f0c280
muchcauliflower <mharmikhail.canlas@gmail.com>
2025-06-06 19:47:57 +0800

    Added map
    Added map in mapsPage but still requires online. will Create the offline through tiles once im able to find the libraries

--- lib/pages/maps_page.dart
+++ lib/pages/maps_page.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class MapsPage extends StatelessWidget {
   const MapsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: FlutterMap(
+        options: MapOptions(
+          center: LatLng(10.7202, 122.5621), // Iloilo coordinates
+          zoom: 13.0,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            subdomains: ['a', 'b', 'c'],
+            userAgentPackageName: 'com.example.mapakaon_redo',
+          ),
+        ],
+      ),
+    );
   }
 }
+
