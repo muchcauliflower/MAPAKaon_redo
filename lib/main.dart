649f7fbbc93290d2453fdf7655cddc1f034afa25
muchcauliflower <mharmikhail.canlas@gmail.com>
2025-06-06 16:33:12 +0800

    button for swtiching pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mapakaon_redo/pages/home_page.dart';
+import 'package:mapakaon_redo/pages/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: MainScreen(),
     );
   }
 }
